740e5f103115c2c4963b1e2f61e25f71c8e54491
melvinschultz <melvinschultzdev@gmail.com>
2016-11-21 22:07:02 +0100

    start to implement facebook login, not finish yet

---
+++ app/src/main/java/net/schultzoss_montpellier/melvin/firebaseauthdemo/FacebookAuthActivity.java
@@ -0,0 +1,13 @@
+package net.schultzoss_montpellier.melvin.firebaseauthdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FacebookAuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_facebook_auth);
+    }
+}
--- app/src/main/java/net/schultzoss_montpellier/melvin/firebaseauthdemo/MainActivity.java
+++ app/src/main/java/net/schultzoss_montpellier/melvin/firebaseauthdemo/MainActivity.java
@@ -16,6 +16,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.facebook.FacebookSdk;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
